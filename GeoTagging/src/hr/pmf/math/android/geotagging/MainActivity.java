d6b5889084fd3785b6378e1220abd1955cd3926d
Grubi <grubelic.n@hotmail.com>
2015-02-10 00:27:00 +0100

    DODAO DAO

--- GeoTagging/src/hr/pmf/math/android/geotagging/MainActivity.java
+++ GeoTagging/src/hr/pmf/math/android/geotagging/MainActivity.java
@@ -1,36 +1,61 @@
 package hr.pmf.math.android.geotagging;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends ActionBarActivity {
+	static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+	static final LatLng KIEL = new LatLng(53.551, 9.993);
+	private GoogleMap map;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+				.getMap();
+		Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
+				.title("Hamburg"));
+		Marker kiel = map.addMarker(new MarkerOptions()
+				.position(KIEL)
+				.title("Kiel")
+				.snippet("Kiel is cool")
+				.icon(BitmapDescriptorFactory
+						.fromResource(R.drawable.ic_launcher)));
 
+		// Move the camera instantly to hamburg with a zoom of 15.
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		// Zoom in, animating the camera.
+		map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
--- GeoTagging/src/hr/pmf/math/android/geotagging/dao/DAOProvider.java
+++ GeoTagging/src/hr/pmf/math/android/geotagging/dao/DAOProvider.java
@@ -1,11 +1,17 @@
 package hr.pmf.math.android.geotagging.dao;
 
+import android.content.Context;
+
 public class DAOProvider {
 	static DAO dao;
 	
 	private DAOProvider(){}
 	
-	public static DAO getDAO(){
+	public static DAO getDAO(Context context){
+		if(dao == null){
+			dao = new SQLDAO(new DBAdapter(context));
+			
+		}
 		return dao;
 	}
 }
