44607fdd1de2c4ebc99db17f6226e7d0aea878e9
lockyrd <marinoloncar@hotmail.com>
2015-02-10 01:37:54 +0100

    Main Activity

--- GeoTagging/src/hr/pmf/math/android/geotagging/MainActivity.java
+++ GeoTagging/src/hr/pmf/math/android/geotagging/MainActivity.java
@@ -1,28 +1,27 @@
 package hr.pmf.math.android.geotagging;
 
+import android.R;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
+import android.view.View;
 
 public class MainActivity extends ActionBarActivity {
-	static final LatLng HAMBURG = new LatLng(53.558, 9.927);
-	static final LatLng KIEL = new LatLng(53.551, 9.993);
-	private GoogleMap map;
+	//static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+	//static final LatLng KIEL = new LatLng(53.551, 9.993);
+	//private GoogleMap map;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+		
+		
+		
+		
+		/*map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
 				.getMap();
 		Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
 				.title("Hamburg"));
@@ -37,9 +36,20 @@ public class MainActivity extends ActionBarActivity {
 		map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
 
 		// Zoom in, animating the camera.
-		map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+		map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);*/
 	}
-
+	
+	public void seeButtonClick(View view){
+		Intent i = new Intent(this,AddActivity.class);
+		startActivity(i);
+	}
+	
+	public void addButtonClick(View view){
+		Intent i = new Intent(this,ViewActivity.class);
+		startActivity(i);
+		
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
